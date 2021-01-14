73b8ba5264b78df72b26b58d65d576b3dba93b87
Samuel L Meyers <thecheesecakejunkie@gmail.com>
2021-01-14 10:03:31 -0600

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+const Array = require("./array");
+
+let array = new Array();
+
+array.push(3);
+
+console.log(array);
+
+array.pop();
+
+console.log(array);
+
+array.push('tahuida');
+
+console.log(array.get(0));
+
